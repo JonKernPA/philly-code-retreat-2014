abb59919206a268180ed6612568c785f3d667771
Jon Kern <jonkern@comcast.net>
2014-11-15 15:20:24 -0500

    initial state

--- preston_and_jon_2/model/world.rb
+++ preston_and_jon_2/model/world.rb
@@ -2,14 +2,50 @@ class World
 
   def initialize
     @cells = []
+    @num_rows = 2
+    @num_cols = 2
+
+    row = 0
+    @num_rows.times do |row|
+      a_row = []
+      @num_cols.times do |col|
+        a_row[col] << Cell.new(true)
+      end
+      @cells[row] = a_row
+    end
+    puts cells
+  end
+
+  def num_rows
+    @num_rows
+  end
+
+  def num_cols
+    @num_cols
+  end
+
+  def cell(x,y)
+
   end
 
   def alive_count
-    @cells.select { |c| c.alive? }.count
+    @cells.flatten.select { |c| c.alive? }.count
   end
 
-  def add_cell(cell)
-    @cells << cell
+  def add_cell(cell, coordinates)
+    @cells[coordinates[0], coordinates[1]] = cell
+=begin
+    if @cells.count == 0
+      @cells = [cell]
+    elsif @cells.count == 1
+      if @cells[0].count < 2
+        @cells[0] << cell
+      else
+        @
+      end
+    end
+=end
+
   end
 
   def next_generation
--- preston_and_jon_2/spec/code_retreat_spec.rb
+++ preston_and_jon_2/spec/code_retreat_spec.rb
@@ -1,6 +1,17 @@
 require './model/world'
 require './model/cell'
 RSpec.describe World do
+  it 'is a 2D world of rows and columns' do
+    world = World.new
+    expect(world.num_rows).to be > 0
+    expect(world.num_cols).to be > 0
+  end
+
+  it 'has cells at given 2D coordinates' do
+    world = World.new
+    expect(world.cell(3,4)).to_not be_nil
+  end
+
 
   describe '#alive_count' do
     it 'returns 0 for a new World' do
@@ -28,13 +39,20 @@ RSpec.describe World do
 
       expect(world.alive_count).to eq 0
     end
-  end
 
-  describe '#next_generation' do
-    context 'under populated' do
+    it 'reproduces only when there are three live neighbors' do
+      world = World.new
+      world.add_cell(Cell.new(true), [0,0])
+      world.add_cell(Cell.new(true), [0,1])
+      world.add_cell(Cell.new(true), [1,0])
+      world.add_cell(Cell.new(false), [1,1])
 
+      world.next_generation
+
+      expect(world.alive_count).to eq 4
     end
   end
+
 
 
 end
