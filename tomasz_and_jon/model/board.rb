089fdc295e04e29f10a0c56008ce0e34822782a1
Jon Kern <jonkern@comcast.net>
2014-12-12 16:58:14 -0500

    crammed in a final run method to demo the logic... forgot to commit along the way :-(

--- tomasz_and_jon/model/board.rb
+++ tomasz_and_jon/model/board.rb
@@ -1,14 +1,92 @@
 class Board
 
   def initialize(initial_state)
+    @state = initial_state
 
   end
 
   def tick
+    rows = @state.count
+    cols = @state.first.count
 
+    new_state = Array.new(rows){Array.new(cols)}
+
+    (1..rows).each do |r|
+      (1..cols).each do |c|
+        current_cell = cell(r,c)
+
+        live_count = live_neighbors(r,c)
+
+        cell_state = 0
+
+        if current_cell == 1
+          # Live
+          # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+          if live_count < 2
+            cell_state = 0
+          end
+          # Any live cell with two or three live neighbours lives on to the next generation.
+          if live_count == 2 || live_count == 3
+            cell_state = 1
+          end
+          # Any live cell with more than three live neighbours dies, as if by overcrowding.
+          if live_count > 3
+            cell_state = 0
+          end
+
+        else
+          # Dead
+          # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+          if live_count == 3
+            cell_state = 1
+          end
+        end
+        new_state[r - 1][c - 1] = cell_state
+                                                                                                                                                                     end
+    end
+    @state = new_state
+  end
+
+  def cell(cell_row, cell_col)
+    @state[cell_row - 1][cell_col - 1]
+  end
+
+  # Humans enter 1-based rows and cols
+  def live_neighbors(cell_row, cell_col)
+    count = 0
+    row = cell_row - 1
+    col = cell_col - 1
+
+    min_row = [row - 1, 0].max
+    max_row = [row + 1, @state.count - 1].min
+    min_col = [col - 1, 0].max
+    max_col = [col + 1, @state.first.count - 1].min
+
+    (min_row..max_row).each do |r|
+      (min_col..max_col).each do |c|
+        # puts "for #{r}:#{c} ==> #{@state[r][c]}"
+        count += @state[r][c]
+      end
+    end
+    count - @state[row][col]
   end
 
   def state
-    [[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]
+    @state
+  end
+
+  def run(evolutions)
+    evolutions.times.each do |i|
+      puts "- "*evolutions
+      sleep 1
+      tick
+      @state.count.times.each do |r|
+        text = ''
+        @state.first.count.times.each do |c|
+          text += @state[r][c] == 0 ? ' ' : '*'
+        end
+        puts text
+      end
+    end
   end
 end
--- tomasz_and_jon/spec/board_spec.rb
+++ tomasz_and_jon/spec/board_spec.rb
@@ -1,29 +1,181 @@
 require './model/board'
 RSpec.describe Board do
+  describe 'Board' do
+    it "can be initialized with a 2D array" do
+      # Given
+      initial_state = [
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+      ]
+      board = Board.new(
+          initial_state
+      )
+      # When/Then
+      expect(board.state).to eq(initial_state)
+
+    end
+
+    describe '#rules' do
+      before(:each) do
+        # Given
+        initial_state = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        @board = Board.new(
+            initial_state
+        )
+      end
+
+      # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+      it 'Rule #1 under-population' do
+        # When
+        @board.tick
+        expect(@board.cell(3, 2)).to eq(0)
+      end
+
+      # Any live cell with more than three live neighbours dies, as if by overcrowding.
+      it 'Rule #2 overcrowding' do
+        # Given
+        initial_state = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        board = Board.new(
+            initial_state
+        )
+        # When
+        board.tick
+        expect(board.cell(3, 3)).to eq(0)
+      end
+
+      # Any live cell with two or three live neighbours lives on to the next generation.
+      it 'Rule #3 lives on' do
+        # Given
+        initial_state = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        board = Board.new(
+            initial_state
+        )
+        # When
+        board.tick
+        expect(board.cell(3, 3)).to eq(1)
+        expect(board.cell(3, 2)).to eq(1)
+      end
+
+      # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+      it 'Rule #4 reproduction' do
+        # When
+        @board.tick
+        expect(@board.cell(2, 3)).to eq(1)
+      end
+
+      # Any live cell with fewer than two live neighbours dies,
+      it 'counts live neighbors' do
+        expect(@board.live_neighbors(3, 3)).to eq(2)
+        expect(@board.live_neighbors(3, 2)).to eq(1)
+      end
+
+
+    end
+
+
+  end
+
   describe 'Blinker' do
-    it "oscillates from one to the other form" do
+    it "oscillates from horizontal to vertical form" do
       # Given
       board = Board.new(
-                       [
-                           [0,0,0,0,0],
-                           [0,0,0,0,0],
-                           [0,1,1,1,0],
-                           [0,0,0,0,0],
-                           [0,0,0,0,0],
-                       ]
+          [
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 1, 1, 1, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+          ]
       )
       # When
       board.tick
       # Then
       expected_state =
           [
-              [0,0,0,0,0],
-              [0,0,1,0,0],
-              [0,0,1,0,0],
-              [0,0,1,0,0],
-              [0,0,0,0,0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 0],
           ]
       expect(board.state).to eq(expected_state)
+    end
+
+    it "oscillates from vertical to horizontal form" do
+      # Given
+      board = Board.new(
+          [
+              [0, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 0],
+          ]
+      )
+      # When
+      board.tick
+      # Then
+      expected_state =
+          [
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 1, 1, 1, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+          ]
+      expect(board.state).to eq(expected_state)
+    end
+  end
+  describe 'run' do
+    it 'runs a few ticks' do
+      initial_state = [
+          [0, 0, 0, 0, 0],
+          [0, 0, 1, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 0, 1, 0, 0],
+          [0, 0, 0, 0, 0],
+      ]
+      board = Board.new(
+          initial_state
+      )
+
+      board.run(15)
+    end
+
+    it 'runs a blinker' do
+      initial_state = [
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+      ]
+      board = Board.new(
+          initial_state
+      )
+
+      board.run(5)
     end
   end
 end
